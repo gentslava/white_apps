82ff32232e0868f466737f08d7599ce68709e600
Vyacheslav Shcherbinin <gentslava@mail.ru>
2023-07-12 11:07:47 +0700

    Common ESLint rules

---
+++ eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  plugins: [
+    'react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'default-param-last': 'warn',
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'linebreak-style': 'off',
+    'no-trailing-spaces': ['error', { skipBlankLines: true }],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: {
+          minProperties: 6,
+          multiline: true,
+          consistent: true,
+        },
+        ObjectPattern: {
+          minProperties: 6,
+          multiline: true,
+          consistent: true,
+        },
+        ImportDeclaration: {
+          minProperties: 6,
+          multiline: true,
+          consistent: true,
+        },
+        ExportDeclaration: {
+          minProperties: 6,
+          multiline: true,
+          consistent: true,
+        },
+      },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/no-array-index-key': 'warn',
+    'react/prop-types': 'off',
+    'react/button-has-type': 'off',
+  },
+};
