15ac562fb24e34bdcf472db972c32b1087fa3498
Vyacheslav Shcherbinin <gentslava@mail.ru>
2024-04-12 09:35:42 +0700

    OK login module

---
+++ spider/okLogin.js
@@ -0,0 +1,21 @@
+const emailInput = '#field_email';
+const passwordInput = '#field_password';
+const submitButton = 'input[value="Log in to OK"][type="submit"]';
+
+module.exports = (account) => new Promise(async (resolve, reject) => {
+  await page.waitForSelector(
+    emailInput,
+    { visible: true, timeout: 0 }
+  );
+
+  await page.click(emailInput),
+  await page.type(emailInput, account.login, { delay: 300, })
+
+  await page.click(passwordInput),
+  await page.type(passwordInput, account.password, { delay: 300, }),
+
+  await page.click(submitButton);
+  resolve({
+    img: 'yes',
+  });
+});
